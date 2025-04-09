a565626e531b03a769cb1314db23b534aebf4df9
leoBouzan <leo.bouzan@outlook.com>
2025-04-09 19:04:47 -0300

    fix: change index to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+import { Box } from "@mui/material";
+import { CardLogin } from "@/components/card-login";
+import { text } from "stream/consumers";
 
-export default function Home (){
+export default function Home() {
   return (
-    <>
-      <h1>Olá, Next.js está funcionando!</h1>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 12,
+          p: 3,
+          borderRadius: 8,
+          width: "fit-content",
+        }}
+      >
+        <h1 style={{ color: '#FFDA00' }}>SINAPSE</h1>
+        <div>
+          <CardLogin />
+        </div>
+      </Box>
+    </Box>
   );
 }
