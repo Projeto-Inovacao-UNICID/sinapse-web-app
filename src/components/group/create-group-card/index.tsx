541f52c85c611bba475095fa02fcbc0d4431148b
leoBouzan <leo.bouzan@outlook.com>
2025-04-27 23:57:01 -0300

    Update index.tsx

--- src/components/group/create-group-card/index.tsx
+++ src/components/group/create-group-card/index.tsx
@@ -1,47 +1,18 @@
-import React, { useState } from 'react';
-import { Button, Box, Typography, TextField, CircularProgress } from '@mui/material';
-import { motion } from 'framer-motion';
 import { usePostGroup } from '@/hooks/group/useGroup';
-import { useFriendship } from '@/hooks/friendship/useFriendship';
+import { Alert, Box, Button, TextField, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const CreateGroupCard = () => {
   const [nome, setNome] = useState('');
   const [descricao, setDescricao] = useState('');
   const [isPublic, setIsPublic] = useState(false);
-  const [selectedUsers, setSelectedUsers] = useState<{ userId: string, nome: string }[]>([]);
 
-  const { data: friends, isLoading, error } = useFriendship();
-  const { mutate: createGroup, isPending } = usePostGroup();
+  const { mutate: createGroup, isPending, isSuccess, isError } = usePostGroup();
 
   const handleCreateGroup = () => {
     createGroup({ nome, descricao, isPublic });
   };
-
-  const handleUserSelect = (userId: string, nome: string) => {
-    if (!selectedUsers.some(user => user.userId === userId)) {
-      setSelectedUsers([...selectedUsers, { userId, nome }]);
-    }
-  };
-
-  const handleUserRemove = (userId: string) => {
-    setSelectedUsers(selectedUsers.filter(user => user.userId !== userId));
-  };
-
-  if (isLoading) {
-    return (
-      <div className="bg-card p-6 rounded-lg shadow-md max-w-sm mx-auto h-full flex justify-center items-center">
-        <CircularProgress sx={{ color: "var(--muted)" }} />
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="bg-card p-6 rounded-lg shadow-md max-w-sm mx-auto">
-        <Typography variant="h6" color="error">Erro ao carregar amigos.</Typography>
-      </div>
-    );
-  }
 
   return (
     <motion.div
@@ -54,6 +25,14 @@ const CreateGroupCard = () => {
         <Typography variant="h6" gutterBottom className="text-primary font-semibold">
           Criar Grupo
         </Typography>
+
+        {/* Exibição das mensagens de sucesso ou erro */}
+        {isSuccess && (
+          <Alert severity="success">Grupo criado com sucesso!</Alert>
+        )}
+        {isError && (
+          <Alert severity="error">Erro ao criar grupo. Tente novamente.</Alert>
+        )}
 
         <TextField
           label="Nome do Grupo"
@@ -142,80 +121,6 @@ const CreateGroupCard = () => {
           </Typography>
         </Box>
 
-        <TextField
-          label="Convite de Amigos"
-          fullWidth
-          margin="normal"
-          className="mb-4"
-          slotProps={{
-            htmlInput: {
-              style: { 
-                background: 'var(--input)', 
-                borderRadius: 'var(--radius)', 
-                color: 'var(--foreground)',
-              },
-            },
-          }}
-          sx={{
-            '& .MuiOutlinedInput-root': {
-              '&:hover fieldset': {
-                borderColor: 'var(--primary)',
-              },
-              '&.Mui-focused fieldset': {
-                borderColor: 'var(--primary)',
-              },
-            },
-            '& .MuiInputLabel-root': {
-              color: 'var(--muted)',
-              '&.Mui-focused': {
-                color: 'var(--primary)',
-              },
-            },
-          }}
-        />
-
-        <Box display="flex" flexWrap="wrap" mb={2}>
-          {friends?.map(friend => (
-            <Button
-              key={friend.usuarioId}
-              variant="outlined"
-              onClick={() => handleUserSelect(friend.usuarioId, friend.nome)}
-              sx={{ marginRight: 1, marginBottom: 1 }}
-              className="text-accent border-accent hover:bg-accent-foreground"
-              style={{
-                borderRadius: 4,
-                color: 'var(--foreground)',
-                borderColor: 'var(--primary)',
-              }}
-            >
-              {friend.nome}
-            </Button>
-          ))}
-        </Box>
-
-        <Box mt={2}>
-          <Typography variant="body2" sx={{color: 'var(--muted)'}}>Usuários selecionados</Typography>
-          <Box display="flex" flexWrap="wrap">
-            {selectedUsers.map(({ userId, nome }) => (
-              <Button
-                key={userId}
-                variant="contained"
-                color="secondary"
-                onClick={() => handleUserRemove(userId)}
-                sx={{ margin: 1 }}
-                className="bg-destructive text-destructive-foreground"
-                style={{
-                  backgroundColor: 'var(--destructive)',
-                  color: 'var(--destructive-foreground)',
-                  borderRadius: 'var(--radius)',
-                }}
-              >
-                {nome} X 
-              </Button>
-            ))}
-          </Box>
-        </Box>
-
         <Box mt={2}>
           <Button
             variant="contained"
@@ -241,7 +146,6 @@ const CreateGroupCard = () => {
           >
             {isPending ? 'Criando...' : 'Criar Grupo'}
           </Button>
-
         </Box>
       </div>
     </motion.div>
