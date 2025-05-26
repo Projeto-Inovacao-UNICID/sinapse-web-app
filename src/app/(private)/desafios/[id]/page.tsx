ecd681b67e623469862bbf6b173ed4fcdcd59311
leoBouzan <leo.bouzan@outlook.com>
2025-05-26 18:29:17 -0300

    Update page.tsx

--- src/app/(private)/desafios/[id]/page.tsx
+++ src/app/(private)/desafios/[id]/page.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import ChallengeDetailPage from '@/components/challenge/detail/challenge-detail-page';
 
 interface PageProps {
-  params: { id: number };
+  params: { id: string };
 }
 
 export default function DesafioPage({ params }: PageProps) {
-  return <ChallengeDetailPage id={params.id} />;
+  const { id } = params;
+  const numberId = Number(id);
+  return <ChallengeDetailPage id={numberId} />;
 }
